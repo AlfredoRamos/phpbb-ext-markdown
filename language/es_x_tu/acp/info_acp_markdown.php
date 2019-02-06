3644e7d43560e9a7b524bf2af5811992cd24ad21
Alfredo Ramos <alfredo.ramos.sanchez@gmail.com>
2019-02-06 01:03:42 -0600

    Add Spanish translations and small fixes in English translation

---
+++ language/es_x_tu/acp/info_acp_markdown.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * Markdown extension for phpBB.
+ * @author Alfredo Ramos <alfredo.ramos@yandex.com>
+ * @copyright 2019 Alfredo Ramos
+ * @license GPL-2.0-only
+ */
+
+/**
+ * @ignore
+ */
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+
+/**
+ * @ignore
+ */
+if (empty($lang) || !is_array($lang))
+{
+	$lang = [];
+}
+
+$lang = array_merge($lang, [
+	'ALLOW_MARKDOWN' => 'Permitir Markdown',
+	'ALLOW_SIG_MARKDOWN' => 'Permitir Markdown en la firma del usuario'
+]);
---
+++ language/es_x_tu/posting.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * Markdown extension for phpBB.
+ * @author Alfredo Ramos <alfredo.ramos@yandex.com>
+ * @copyright 2019 Alfredo Ramos
+ * @license GPL-2.0-only
+ */
+
+/**
+ * @ignore
+ */
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+
+/**
+ * @ignore
+ */
+if (empty($lang) || !is_array($lang))
+{
+	$lang = [];
+}
+
+$lang = array_merge($lang, [
+	'MARKDOWN_STATUS_FORMAT' => '<a href="%s">Markdown</a> está <em>%s</em>',
+	'MARKDOWN_IS_ON' => 'habilitado',
+	'MARKDOWN_IS_OFF' => 'deshabilitado',
+	'DISABLE_MARKDOWN' => 'Deshabilitar Markdown'
+]);
